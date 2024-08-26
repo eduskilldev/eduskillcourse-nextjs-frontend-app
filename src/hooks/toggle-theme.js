033920cfc14d5fcaecbd7fe0e24b6b51fb31f0e2
Bforbilly24 <halimputra0701@gmail.com>
2024-08-26 18:26:30 +0700

    feat(hook): add theme toggler

--- src/hooks/toggle-theme.js
+++ src/hooks/toggle-theme.js
@@ -1,22 +1,31 @@
 'use client';
 
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
+import * as React from 'react';
 import { Button } from '@/components/shadcn/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/shadcn/ui/dropdown-menu';
+import { MoonIcon } from '@/components/svgs/moon-icon';
+import { SunIcon } from '@/components/svgs/sun-icon';
 
 function ToggleTheme() {
-	const { theme, setTheme } = useTheme();
+	const { setTheme } = useTheme();
 
 	return (
-		<div className='flex w-full gap-5'>
-			<Button className='w-full bg-primary' onClick={() => setTheme('light')}>
-				<SunIcon className='h-4 w-4' />
-			</Button>
-			<Button className='group w-full bg-white dark:bg-primary-foreground dark:hover:bg-primary-foreground/90' onClick={() => setTheme('dark')}>
-				<MoonIcon className='h-4 w-4 text-primary group-hover:text-primary-foreground dark:text-primary group-hover:dark:text-primary' />
-			</Button>
-		</div>
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant='outline' size='icon'>
+					<SunIcon className='fill-black h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+					<MoonIcon className='fill-white absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+					<span className='sr-only'>Toggle theme</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align='end'>
+				<DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
 }
 
-export { ToggleTheme };
+export default ToggleTheme;
